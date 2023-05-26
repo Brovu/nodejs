021f0fae51e9009f804a269dadaad758b94c29b3
Brovu <roy772003@gmail.com>
2023-05-26 22:25:39 +0700

    change

--- src/index.js
+++ src/index.js
@@ -6,17 +6,13 @@ const path = require("path");
 const port = 3000;
 const methodOverride = require("method-override");
 const sortMiddleware = require("./app/middleware/SortMiddleware");
-const UserModel = require("./app/model/User");
 const route = require("./routes");
-const jwt = require("jsonwebtoken");
-const cookieParser = require("cookie-parser");
 const db = require("./config/db");
 
 //Connect to DB
 db.connect();
 
 //Apply midleware
-app.use(cookieParser());
 app.use(
   express.urlencoded({
     extended: true,
@@ -66,57 +62,6 @@ app.engine(
 );
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "resources\\views"));
-
-/* app.get("/login", (req, res) => {
-  res.render("auth/login", { showHeader: false, showFooter: false });
-});
-
-app.post("/login", (req, res, next) => {
-  const username = req.body.username;
-  const password = req.body.password;
-
-  UserModel.findOne({
-    username: username,
-    password: password,
-  })
-    .then((data) => {
-      if (data) {
-        const token = jwt.sign(
-          {
-            _id: data._id,
-          },
-          "pass"
-        );
-        return res.json({
-          message: "Success!!",
-          token: token,
-        });
-      } else {
-        return res.json("Fail!!");
-      }
-    })
-    .catch((err) => {
-      res.status(500).json(err);
-    });
-});
-
-app.get(
-  "/:token",
-  (req, res, next) => {
-    try {
-      const token = req.cookies.token;
-      const result = jwt.verify(token, "pass");
-      if (result) {
-        next();
-      }
-    } catch (err) {
-      res.json("Bạn cần phải đăng nhập");
-    }
-  },
-  (req, res, next) => {
-    res.json("Welcome");
-  }
-); */
 
 //Route init
 route(app);
